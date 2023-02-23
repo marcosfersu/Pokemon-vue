05688822114c5bffe25ff8c6814ad9283ad9dcd4
Marcos Fernández <marcos.fersu@gmail.com>
2023-02-23 15:25:03 -0300

    fix mobile list

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,18 +1,18 @@
 <template>
-	<h1>pokemon list</h1>
-	<div class="container">
-		<div class="poke-list">
-			<PokeCard
-				v-for="poke in pokes"
-				:key="poke.id"
-				:name="poke.name"
-				:nameJp="poke.nameJp"
-				:sprites="poke.sprites"
-				:id="poke.id"
-				:types="poke.types"
-			/>
-		</div>
-	</div>
+  <h1>pokemon list</h1>
+  <div class="container">
+    <div class="poke-list">
+      <PokeCard
+        v-for="poke in pokes"
+        :key="poke.id"
+        :name="poke.name"
+        :nameJp="poke.nameJp"
+        :sprites="poke.sprites"
+        :id="poke.id"
+        :types="poke.types"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -23,17 +23,23 @@ const pokeActions = usePokeStore();
 const pokes = pokeActions.pokeStore;
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .container {
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 .poke-list {
-	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(19em, 1fr));
-	gap: 4em;
-	align-items: stretch;
-	max-width: 100%;
-	margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(19em, 1fr));
+  gap: 4em;
+  align-items: stretch;
+  max-width: 100%;
+  margin: 0 auto;
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
