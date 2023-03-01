2883b2814a6f0eca35584493195ac6b9c8c94d95
marcos.fersu <marcos.fersu@gmail.com>
2023-03-01 15:18:28 -0300

    fix flaver text poke detail

--- src/components/EvoItem.vue
+++ src/components/EvoItem.vue
@@ -1,29 +1,29 @@
 <template>
-  <div v-if="evoData" class="evo-chain-poke">
-    <div v-if="evoData.minLevel > 1" class="evo-info">
-      <p class="lvl-poke">lvl {{ evoData.minLevel }}</p>
-      <ArrowEvo />
-    </div>
-    <div v-if="evoData.triggerName === `trade`" class="evo-info">
-      <p class="lvl-poke">
-        {{ evoData.triggerName }}
-      </p>
-      <ArrowEvo />
-    </div>
-    <div v-if="evoData.stone" class="evo-info">
-      <div class="lvl-poke">
-        <img :src="evoData.stone" alt="" />
-      </div>
-      <ArrowEvo />
-    </div>
-    <router-link
-      :to="{ name: `detailPoke`, params: { id: evoData.id } }"
-      class="evo-chain-img"
-    >
-      <img :src="evoData.sprite" alt="" />
-      <p>{{ evoData.speciesName }}</p>
-    </router-link>
-  </div>
+	<div v-if="evoData" class="evo-chain-poke">
+		<div v-if="evoData.minLevel > 1" class="evo-info">
+			<p class="lvl-poke">lvl {{ evoData.minLevel }}</p>
+			<ArrowEvo />
+		</div>
+		<div v-if="evoData.triggerName === `trade`" class="evo-info">
+			<p class="lvl-poke">
+				{{ evoData.triggerName }}
+			</p>
+			<ArrowEvo />
+		</div>
+		<div v-if="evoData.stone" class="evo-info">
+			<div class="lvl-poke">
+				<img :src="evoData.stone" alt="" />
+			</div>
+			<ArrowEvo />
+		</div>
+		<router-link
+			:to="{ name: `detailPoke`, params: { id: evoData.id } }"
+			class="evo-chain-img"
+		>
+			<img :src="evoData.sprite" alt="" />
+			<p>{{ evoData.speciesName }}</p>
+		</router-link>
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -32,56 +32,56 @@ import { IevoData } from "@/data";
 import { defineProps, PropType } from "vue";
 
 defineProps({
-  evoData: {
-    type: Object as PropType<IevoData>,
-  },
+	evoData: {
+		type: Object as PropType<IevoData>,
+	},
 });
 </script>
 <style lang="scss" scoped>
 .evo-chain-poke {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  @media screen and (max-width: 550px) {
-    flex-direction: column;
-  }
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	@media screen and (max-width: 550px) {
+		flex-direction: column;
+	}
 
-  .evo-info {
-    position: relative;
-    @media screen and (max-width: 550px) {
-      height: 4em;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-top: 1em;
-    }
-    svg {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      z-index: 1;
-    }
-    .lvl-poke {
-      margin: 0 1em;
-      min-width: 3.5em;
-      z-index: 2;
-      position: relative;
+	.evo-info {
+		position: relative;
+		@media screen and (max-width: 550px) {
+			height: 4em;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			margin-top: 1em;
+		}
+		svg {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			z-index: 1;
+		}
+		.lvl-poke {
+			margin: 0 1em;
+			min-width: 3.5em;
+			z-index: 2;
+			position: relative;
 
-      img {
-        width: 2.5em;
-      }
-    }
-  }
+			img {
+				width: 2.5em;
+			}
+		}
+	}
 
-  .evo-chain-img {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+	.evo-chain-img {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 
-    p {
-      margin: 0;
-    }
-  }
+		p {
+			margin: 0;
+		}
+	}
 }
 </style>
--- src/data/types.ts
+++ src/data/types.ts
@@ -4,71 +4,82 @@ export type Ingredients = (typeof IngredientNames)[number];
 export type Position = [number, number];
 
 export interface CellInfo {
-  id: string;
-  type: number;
-  position: Position;
+	id: string;
+	type: number;
+	position: Position;
 }
 export interface PokeTypes {
-  type: {
-    name: string;
-  };
+	type: {
+		name: string;
+	};
 }
 export interface PokeStatsApi {
-  base_stat: number;
-  stat: {
-    name: string;
-  };
+	base_stat: number;
+	stat: {
+		name: string;
+	};
 }
 export interface PokeAbilitiesApi {
-  is_hidden: boolean;
-  ability: {
-    name: string;
-  };
+	is_hidden: boolean;
+	ability: {
+		name: string;
+	};
 }
 export interface PokeAbilities {
-  isHidden: boolean;
-  name: string;
+	isHidden: boolean;
+	name: string;
 }
 export interface PokeStats {
-  baseStat: number;
-  name: string;
+	baseStat: number;
+	name: string;
 }
 
 export interface PokeEvo {
-  speciesName: string;
-  minLevel: number;
-  triggerName: string | null;
-  item: { name: string; url: string } | null;
-  sprite?: string;
-  stone?: string;
-  id?: number;
+	speciesName: string;
+	minLevel: number;
+	triggerName: string | null;
+	item: { name: string; url: string } | null;
+	sprite?: string;
+	stone?: string;
+	id?: number;
 }
 
 export interface IevoData {
-  speciesName: string;
-  minLevel: number;
-  triggerName: string | null;
-  item: { name: string; url: string } | null;
-  sprite?: string;
-  stone?: string;
-  id?: number;
+	speciesName: string;
+	minLevel: number;
+	triggerName: string | null;
+	item: { name: string; url: string } | null;
+	sprite?: string;
+	stone?: string;
+	id?: number;
+}
+
+export interface IflavorText {
+	flavor_text: string;
+	language: {
+		name: string;
+	};
+}
+
+export interface IextraData {
+	flavor_text_entries: IflavorText[];
 }
 
 export interface PokeInfo {
-  id: number;
-  name: string;
-  nameJp: string;
-  flavorText: string;
-  height: number;
-  weight: number;
-  sprites: string;
-  types: string[];
-  evoId: number;
-  officialArtwork: string;
-  officialArtworkShiny: string;
-  stats: PokeStats[];
-  abilities: string[];
-  abilitiesHidden: string[];
+	id: number;
+	name: string;
+	nameJp: string;
+	flavorText: string;
+	height: number;
+	weight: number;
+	sprites: string;
+	types: string[];
+	evoId: number;
+	officialArtwork: string;
+	officialArtworkShiny: string;
+	stats: PokeStats[];
+	abilities: string[];
+	abilitiesHidden: string[];
 }
 
 export type BoardState = (CellInfo | null)[][];
@@ -76,31 +87,6 @@ export type BoardState = (CellInfo | null)[][];
 export type PokeStoreInfo = PokeInfo[];
 
 export type EvoStoreInfo = {
-  id: number;
-  data: (PokeEvo | PokeEvo[])[];
+	id: number;
+	data: (PokeEvo | PokeEvo[])[];
 }[];
-
-export type OrthDirection = 0 | 1 | 2 | 4 | 8;
-export interface LineDef {
-  cell: CellInfo;
-  direction: OrthDirection;
-}
-
-export interface RecipeResponse {
-  success: boolean;
-  type?: "request" | "match";
-  payload?: number;
-}
-
-export type SkillNames = "recipelimit" | "boardsize";
-
-export interface SkillInfoBase {
-  name: SkillNames;
-  prices: number[];
-  description: string;
-}
-
-export interface PxLocation {
-  xPos: string;
-  yPos: string;
-}
