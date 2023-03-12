296eac109099e93a66ed938c83068ed9e1158714
Marcos Fernández <marcos.fersu@gmail.com>
2023-03-12 12:33:09 -0300

    add evo change in utils

--- src/store/evoStore.ts
+++ src/store/evoStore.ts
@@ -4,10 +4,19 @@ import { EvoStoreInfo, PokeEvo } from "../data";
 
 export const useEvoStore = defineStore("evo", () => {
   const EvoStore: Ref<EvoStoreInfo> = ref([]);
+  const EvoStoreSprite: Ref<EvoStoreInfo> = ref([]);
 
-  const allEvoStore = (evo: { id: number; data: (PokeEvo | PokeEvo[])[] }) => {
+  const allEvoStore = (evo: any) => {
     EvoStore.value.push(evo);
     EvoStore.value.sort(function (x, y) {
+      return x.id - y.id;
+    });
+  };
+  const allEvoStoreSprite = (evo: any) => {
+    console.log(evo);
+
+    EvoStoreSprite.value.push(evo);
+    EvoStoreSprite.value.sort(function (x, y) {
       return x.id - y.id;
     });
   };
@@ -36,6 +45,8 @@ export const useEvoStore = defineStore("evo", () => {
     allEvoStore,
     getCurrentEvo,
     EvoStore,
+    allEvoStoreSprite,
+    EvoStoreSprite,
     checkEvo,
   };
 });
